3e88df972ab0f3af418d33cf13dcd3f3169b5b23
Rolf Rahm <rahm@fes-es.de>
2025-04-13 00:40:29 +0200

    Update communication.cpp

--- communication.cpp
+++ communication.cpp
@@ -18,57 +18,41 @@
 // ****** Initialiserung I2C-Bus **********************************************
 void i2c_init (void)
 {
-	#ifdef _ATMEGA8_
-		#error not yet implemented for _ATMEGA8_ :(
-	#endif
-
-	#ifdef _ATMEGA328_
-		#ifdef _ATMEGA328PB_
-		  TWSR0 &= ~((1<<TWPS0)|(1<<TWPS1));	// Prescaler = 1
-		  TWBR0 = ((F_CPU / _I2C_FREQUENZ_) - 16) / 2;
-		#else
-		  TWSR &= ~((1<<TWPS0)|(1<<TWPS1));	// Prescaler = 1
-		  TWBR = ((F_CPU / _I2C_FREQUENZ_) - 16) / 2;
-		#endif
+	#ifdef _ATMEGA328PB_
+	  TWSR0 &= ~((1<<TWPS0)|(1<<TWPS1));	// Prescaler = 1
+	  TWBR0 = ((F_CPU / _I2C_FREQUENZ_) - 16) / 2;
+	#else
+	  TWSR &= ~((1<<TWPS0)|(1<<TWPS1));	// Prescaler = 1
+	  TWBR = ((F_CPU / _I2C_FREQUENZ_) - 16) / 2;
 	#endif
 }
 
 // ****** Startbedingung I2C-Bus **********************************************
 void i2c_start (void)
 {
-	#ifdef _ATMEGA8_
-		#error not yet implemented for _ATMEGA8_ :(
-	#endif
-	#ifdef _ATMEGA328_
-		#ifdef _ATMEGA328PB_
-			/*writing a one to TWINT clears it, TWSTA=Start, TWEN=TWI-enable*/
-			TWCR0 = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);		// Control Register
-			/*wait, until start condition has been sent --> ACK*/
-			while (!(TWCR0 & (1<<TWINT)));
-		#else
-			/*writing a one to TWINT clears it, TWSTA=Start, TWEN=TWI-enable*/
-			TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);		// Control Register
-			/*wait, until start condition has been sent --> ACK*/
-			while (!(TWCR & (1<<TWINT)));
-		#endif
+	#ifdef _ATMEGA328PB_
+		/*writing a one to TWINT clears it, TWSTA=Start, TWEN=TWI-enable*/
+		TWCR0 = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);		// Control Register
+		/*wait, until start condition has been sent --> ACK*/
+		while (!(TWCR0 & (1<<TWINT)));
+	#else
+		/*writing a one to TWINT clears it, TWSTA=Start, TWEN=TWI-enable*/
+		TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);		// Control Register
+		/*wait, until start condition has been sent --> ACK*/
+		while (!(TWCR & (1<<TWINT)));
 	#endif
 }
 
 //****** Stoppbedingung I2C-Bus ***********************************************************
 void i2c_stop (void)
 {
-	#ifdef _ATMEGA8_
-		#error not yet implemented for _ATMEGA8_ :(
-	#endif
-	#ifdef _ATMEGA328_
-  	#ifdef _ATMEGA328PB_
-  	  /*writing a one to TWINT clears it, TWSTO=Stop, TWEN=TWI-enable*/
-  	  TWCR0 = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
-    #else
-		  /*writing a one to TWINT clears it, TWSTO=Stop, TWEN=TWI-enable*/
-		  TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
-    #endif
-	#endif
+	#ifdef _ATMEGA328PB_
+    /*writing a one to TWINT clears it, TWSTO=Stop, TWEN=TWI-enable*/
+    TWCR0 = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+  #else
+	  /*writing a one to TWINT clears it, TWSTO=Stop, TWEN=TWI-enable*/
+	  TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+  #endif
 }
 
 //*****************************************************************************************
@@ -76,34 +60,29 @@ void i2c_stop (void)
 // ****************************************************************************************
 uint8_t i2c_write (uint8_t byte)
 {
-  #ifdef _ATMEGA8_
-		#error not yet implemented for _ATMEGA8_ :(
+	#ifdef _ATMEGA328PB_
+    /*TWDR contains byte to send*/
+    TWDR0 = byte;
+    /*send content of TWDR*/
+    TWCR0 = (1<<TWINT) | (1<<TWEN);
+    /*wait, until byte has been sent --> ACK*/
+    while (!(TWCR0 & (1<<TWINT)));
+    // Abfrage der TWI-Statuscodes (ATmega128 Manual S.214)
+    if (((TWSR0 & 0xf8)== 0x18) || ((TWSR0 & 0xf8)== 0x28)) return ACK;
+    if (((TWSR0 & 0xf8)== 0x20) || ((TWSR0 & 0xf8)== 0x30)) return NACK;
+    return 0;	//Status Register
+  #else
+	  /*TWDR contains byte to send*/
+	  TWDR = byte;
+	  /*send content of TWDR*/
+	  TWCR = (1<<TWINT) | (1<<TWEN);
+	  /*wait, until byte has been sent --> ACK*/
+	  while (!(TWCR & (1<<TWINT)));
+	  // Abfrage der TWI-Statuscodes (ATmega128 Manual S.214)
+	  if (((TWSR & 0xf8)== 0x18) || ((TWSR & 0xf8)== 0x28)) return ACK;
+	  if (((TWSR & 0xf8)== 0x20) || ((TWSR & 0xf8)== 0x30)) return NACK;
+	  return 0;	//Status Register
   #endif
-	#ifdef _ATMEGA328_
-    #ifdef _ATMEGA328PB_
-      /*TWDR contains byte to send*/
-      TWDR0 = byte;
-      /*send content of TWDR*/
-      TWCR0 = (1<<TWINT) | (1<<TWEN);
-      /*wait, until byte has been sent --> ACK*/
-      while (!(TWCR0 & (1<<TWINT)));
-      // Abfrage der TWI-Statuscodes (ATmega128 Manual S.214)
-      if (((TWSR0 & 0xf8)== 0x18) || ((TWSR0 & 0xf8)== 0x28)) return ACK;
-      if (((TWSR0 & 0xf8)== 0x20) || ((TWSR0 & 0xf8)== 0x30)) return NACK;
-      return 0;	//Status Register
-    #else
-		  /*TWDR contains byte to send*/
-		  TWDR = byte;
-		  /*send content of TWDR*/
-		  TWCR = (1<<TWINT) | (1<<TWEN);
-		  /*wait, until byte has been sent --> ACK*/
-		  while (!(TWCR & (1<<TWINT)));
-		  // Abfrage der TWI-Statuscodes (ATmega128 Manual S.214)
-		  if (((TWSR & 0xf8)== 0x18) || ((TWSR & 0xf8)== 0x28)) return ACK;
-		  if (((TWSR & 0xf8)== 0x20) || ((TWSR & 0xf8)== 0x30)) return NACK;
-		  return 0;	//Status Register
-    #endif
-	#endif
 }
 
 //*****************************************************************************************
@@ -111,33 +90,28 @@ uint8_t i2c_write (uint8_t byte)
 // ****************************************************************************************
 uint8_t i2c_read (uint8_t ack)
 {
-	#ifdef _ATMEGA8_
-		#error not yet implemented for _ATMEGA8_ :(
-	#endif
-	#ifdef _ATMEGA328_
-  	#ifdef _ATMEGA328PB_
+ 	#ifdef _ATMEGA328PB_
   	if (ack == ACK)
   	  /*send content of TWDR; TWEA = enable ACK*/
-  	  TWCR0 = (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
-  	  else
-  	  /*send content of TWDR; NACK*/
-  	  TWCR0 = (1<<TWINT) | (1<<TWEN);
-  	
-  	  /*wait, until byte has been received --> ACK*/
-  	  while (!(TWCR0 & (1<<TWINT)));
-  	  return TWDR0;
-    #else
-		  if (ack == ACK)
-			  /*send content of TWDR; TWEA = enable ACK*/
-			  TWCR = (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
-		  else 
-			  /*send content of TWDR; NACK*/
-			  TWCR = (1<<TWINT) | (1<<TWEN);
+ 	    TWCR0 = (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
+    else
+ 	    /*send content of TWDR; NACK*/
+ 	    TWCR0 = (1<<TWINT) | (1<<TWEN);
+ 	
+    /*wait, until byte has been received --> ACK*/
+    while (!(TWCR0 & (1<<TWINT)));
+    return TWDR0;
+  #else
+		if (ack == ACK)
+		  /*send content of TWDR; TWEA = enable ACK*/
+		  TWCR = (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
+		else 
+		  /*send content of TWDR; NACK*/
+		  TWCR = (1<<TWINT) | (1<<TWEN);
 			
-		  /*wait, until byte has been received --> ACK*/
-		  while (!(TWCR & (1<<TWINT)));
-		  return TWDR;
-    #endif
+		/*wait, until byte has been received --> ACK*/
+		while (!(TWCR & (1<<TWINT)));
+		return TWDR;
 	#endif
 }
 
@@ -148,64 +122,44 @@ uint8_t i2c_read (uint8_t ack)
 void rs232_init(void)
 { 
   rs232_baud(BAUD);
-  //// Hilfsmakro zur UBRR-Berechnung ("Formel" laut Datenblatt)
-  //#define UART_UBRR_CALC(BAUD_,FREQ_) ((FREQ_)/((BAUD_)*16L)-1)
-  //#ifdef _ATMEGA8_
-   //UCSRB |= (1<<TXEN) | (1<<RXEN);    // UART TX und RX einschalten
-   //UCSRC |= (1<<URSEL)|(3<<UCSZ0);    // Asynchron 8N1
-   //UBRRH = (uint8_t)( UART_UBRR_CALC( BAUD, F_CPU ) >> 8 );
-   //UBRRL = (uint8_t)  UART_UBRR_CALC( BAUD, F_CPU );
-  //#endif
-  //#ifdef _ATMEGA328_
-   //UCSR0B |= (1<<TXEN0) | (1<<RXEN0);    // UART TX und RX einschalten
-   //UCSR0C |= (1<<USBS0) | (3<<UCSZ00);   // Asynchron 8N1
-   //UBRR0H = (uint8_t)( UART_UBRR_CALC( BAUD, F_CPU ) >> 8 );
-   //UBRR0L = (uint8_t)  UART_UBRR_CALC( BAUD, F_CPU );
-  //#endif
 }
 
-void rs232_baud ( uint32_t baud )     // �ndert die Baudrate
+void rs232_baud(uint32_t baud)
 {
-    #define UART_UBRR_CALC(BAUD_,FREQ_) ((FREQ_)/((BAUD_)*16L)-1)
-    #ifdef _ATMEGA8_
-    UCSRB |= (1<<TXEN) | (1<<RXEN);    // UART TX und RX einschalten
-    UCSRC |= (1<<URSEL)|(3<<UCSZ0);    // Asynchron 8N1
-    UBRRH = (uint8_t)( UART_UBRR_CALC( baud, F_CPU ) >> 8 );
-    UBRRL = (uint8_t)  UART_UBRR_CALC( baud, F_CPU );
-    #endif
-    #ifdef _ATMEGA328_
-       #include <util/setbaud.h>
-       UBRR0H = UBRRH_VALUE;
-       UBRR0L = UBRRL_VALUE;
-    UCSR0B |= (1<<TXEN0) | (1<<RXEN0);    // UART TX und RX einschalten
-    UCSR0C |= (1<<USBS0) | (3<<UCSZ00);   // Asynchron 8N1
-    //UBRR0H = (uint8_t)( UART_UBRR_CALC( baud, F_CPU ) >> 8 );
-    //UBRR0L = (uint8_t)  UART_UBRR_CALC( baud, F_CPU );
-    #endif
-}
+  uint16_t ubrr_value;
+  
+  // Berechne den UBRR-Wert basierend auf der gewünschten Baudrate
+  if (baud >= 57600) 
+  {
+    // Setze das U2X-Bit für höhere Baudraten
+    UCSR0A |= (1 << U2X0);
+    ubrr_value = (F_CPU / (8 * baud)) - 1;
+  } else 
+  {
+    // Lösche das U2X-Bit für niedrigere Baudraten
+    UCSR0A &= ~(1 << U2X0);
+    ubrr_value = (F_CPU / (16 * baud)) - 1;
+  }
+  
+  // Setze die UBRR-Register
+  UBRR0H = (ubrr_value >> 8);
+  UBRR0L = ubrr_value;
+
+  UCSR0B |= (1<<TXEN0) | (1<<RXEN0);    // UART TX und RX einschalten
+  UCSR0C |= (1<<USBS0) | (3<<UCSZ00);   // Asynchron 8N1
+  }
+  
 
 uint8_t rs232_get ( void )
 {
-  #ifdef _ATMEGA8_
-   if (!(UCSRA & (1<<RXC))) return ('\0');    // Wenn kein Zeichen im Puffer, dann 0 zur�ckgeben!
-   return UDR;                                // Zeichen zur�ckliefern
-  #endif
-  #ifdef _ATMEGA328_
-   if (!(UCSR0A & (1<<RXC0))) return ('\0');    // Wenn kein Zeichen im Puffer, dann 0 zur�ckgeben!
-   return UDR0;                                // Zeichen zur�ckliefern
-  #endif
-}
+  if (!(UCSR0A & (1<<RXC0))) return ('\0');    // Wenn kein Zeichen im Puffer, dann 0 zurückgeben!
+  return UDR0;                                // Zeichen zurückliefern
+ }
 
 void rs232_put ( uint8_t value )
 {
-  #ifdef _ATMEGA8_
-   while (!(UCSRA & (1<<UDRE)));              // Warten bis Sendebereit!
-   UDR = value;
-  #endif
-  #ifdef _ATMEGA328_
-   while (!(UCSR0A & (1<<UDRE0)));              // Warten bis Sendebereit!
-   UDR0 = value;
-  #endif
+  while (!(UCSR0A & (1<<UDRE0)));              // Warten bis Sendebereit!
+  UDR0 = value;
 }
 
 // Doppelte Definition notwendig, sonst Fehlermeldung "unerlaubte Typumwandlung"
@@ -267,7 +221,7 @@ void rs232_int(uint16_t val)
     buffer[n++] = val%10 + '0';
   } while ((val /= 10) > 0);
   
-  while (n<5)                   // Rest von buffer mit blank f�llen
+  while (n<5)                   // Rest von buffer mit blank füllen
   {
     buffer[n++] = ' ';
   }
@@ -283,7 +237,7 @@ uint8_t rs232_binary_get ( void )
 {
    while (!rs232_is_received());    // warten, bis Byte im Puffer!
 
-   return UDR0;                     // wert zur�ckgeben
+   return UDR0;                     // wert zurückgeben
 }
 
 uint8_t rs232_readbytes(uint8_t *buf, int8_t _length)
@@ -291,7 +245,7 @@ uint8_t rs232_readbytes(uint8_t *buf, int8_t _length)
    uint16_t timeout = 0;
    uint8_t  i = 0;
 
-   //Puffer l�schen
+   //Puffer löschen
    for (uint8_t n=0; n<=_length;n++)  buf[n] = 0;
 
    while(_length > 0)
@@ -312,9 +266,6 @@ uint8_t rs232_readbytes(uint8_t *buf, int8_t _length)
 
 uint8_t rs232_is_received( void )
 {
-    #ifdef _ATMEGA328_
-    if (!(UCSR0A & (1<<RXC0))) return 0;    // 0, wenn kein Byte im Puffer!
-
-    return 1;                      // 1, wenn ein Byte im Puffer
-    #endif
+  if (!(UCSR0A & (1<<RXC0))) return 0;    // 0, wenn kein Byte im Puffer!
+  return 1;                               // 1, wenn ein Byte im Puffer
 }
